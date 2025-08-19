779527ca420fb3f44196a992ecc2102908a78fc5
jamunesh15 <jsheta15@gmail.com>
2025-08-19 13:56:42 +0530

    change from branch jemu

---
+++ a.js
@@ -0,0 +1,28 @@
+
+let str = "my name is jemu"
+
+let arr = str.trim();
+
+let count = 0;
+for(let  i =0 ; i<arr.length;i++){
+      if(arr[i] === " ") {
+           console.log("space he");
+           
+      }
+      else{
+         count++; 
+      }
+}
+
+ console.log("arr" , arr);
+
+ console.log("jemu have changed over here");
+ 
+ 
+
+console.log(count);
+
+
+
+
+
